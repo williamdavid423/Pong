5d4146f3b1302552c4dd6e07b9e9d51806c684ff
billy david <williamdavid423@hotmail.com>
2021-03-09 10:36:43 -0500

    Add project files.

---
+++ Pong/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.p1ScoreLabel = new System.Windows.Forms.Label();
+            this.p2ScoreLabel = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // p1ScoreLabel
+            // 
+            this.p1ScoreLabel.AutoSize = true;
+            this.p1ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.p1ScoreLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p1ScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.p1ScoreLabel.Location = new System.Drawing.Point(176, 9);
+            this.p1ScoreLabel.Name = "p1ScoreLabel";
+            this.p1ScoreLabel.Size = new System.Drawing.Size(0, 19);
+            this.p1ScoreLabel.TabIndex = 0;
+            this.p1ScoreLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.p1ScoreLabel.Click += new System.EventHandler(this.P1ScoreLabel_Click);
+            // 
+            // p2ScoreLabel
+            // 
+            this.p2ScoreLabel.AutoSize = true;
+            this.p2ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.p2ScoreLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p2ScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.p2ScoreLabel.Location = new System.Drawing.Point(375, 9);
+            this.p2ScoreLabel.Name = "p2ScoreLabel";
+            this.p2ScoreLabel.Size = new System.Drawing.Size(0, 19);
+            this.p2ScoreLabel.TabIndex = 1;
+            this.p2ScoreLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(600, 400);
+            this.Controls.Add(this.p2ScoreLabel);
+            this.Controls.Add(this.p1ScoreLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label p1ScoreLabel;
+        private System.Windows.Forms.Label p2ScoreLabel;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ Pong/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pong
+{
+    public partial class Form1 : Form
+    {
+        int paddle1X = 10;
+        int paddle1Y = 170;
+        int player1Score = 0;
+
+        int paddle2X = 580;
+        int paddle2Y = 170;
+        int player2Score = 0;
+
+        int paddleWidth = 10;
+        int paddleHeight = 60;
+        int paddleSpeed = 4;
+
+        int ballX = 295;
+        int ballY = 195;
+        int ballXSpeed = 6;
+        int ballYSpeed = 6;
+        int ballWidth = 10;
+        int ballHeight = 10;
+
+
+
+        bool wDown = false;
+        bool sDown = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+
+
+        SolidBrush blueBrush = new SolidBrush(Color.DodgerBlue);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void P1ScoreLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+
+            }
+        }
+
+            private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+                switch (e.KeyCode)
+                {
+                    case Keys.W:
+                        wDown = false;
+                        break;
+                    case Keys.S:
+                        sDown = false;
+                        break;
+                    case Keys.Up:
+                        upArrowDown = false;
+                        break;
+                    case Keys.Down:
+                        downArrowDown = false;
+                        break;
+                }
+
+            }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(whiteBrush, ballX, ballY, ballWidth, ballHeight);
+
+            e.Graphics.FillRectangle(blueBrush, paddle1X, paddle1Y, paddleWidth, paddleHeight);
+            e.Graphics.FillRectangle(blueBrush, paddle2X, paddle2Y, paddleWidth, paddleHeight);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            //move ball 
+            ballX += ballXSpeed;
+            ballY += ballYSpeed;
+
+            //move player 1 
+            if (wDown == true && paddle1Y > 0)
+            {
+                paddle1Y -= paddleSpeed;
+            }
+
+            if (sDown == true && paddle1Y < this.Height - paddleHeight)
+            {
+                paddle1Y += paddleSpeed;
+            }
+
+            //move player 2 
+            if (upArrowDown == true && paddle2Y > 0)
+            {
+                paddle2Y -= paddleSpeed;
+            }
+
+            if (downArrowDown == true && paddle2Y < this.Height - paddleHeight)
+            {
+                paddle2Y += paddleSpeed;
+            }
+
+            //check if ball hit top or bottom wall and change direction if it does 
+            if (ballY < 0 || ballY > this.Height - ballHeight)
+            {
+                ballYSpeed *= -1;  // or: ballYSpeed = -ballYSpeed; 
+            }
+
+            //create Rectangles of objects on screen to be used for collision detection 
+            Rectangle player1Rec = new Rectangle(paddle1X, paddle1Y, paddleWidth, paddleHeight);
+            Rectangle player2Rec = new Rectangle(paddle2X, paddle2Y, paddleWidth, paddleHeight);
+            Rectangle ballRec = new Rectangle(ballX, ballY, ballWidth, ballHeight);
+
+            //check if ball hits either paddle. If it does change the direction 
+            //and place the ball in front of the paddle hit 
+            if (player1Rec.IntersectsWith(ballRec))
+            {
+                ballXSpeed *= -1;
+                ballX = paddle1X + paddleWidth + 1;
+            }
+            else if (player2Rec.IntersectsWith(ballRec))
+            {
+                ballXSpeed *= -1;
+                ballX = paddle2X - ballWidth - 1;
+            }
+
+            //check if a player missed the ball and if true add 1 to score of other player  
+            if (ballX < 0)
+            {
+                player2Score++;
+
+                p2ScoreLabel.Text = $"{player2Score}";
+
+
+
+                ballX = 295;
+                ballY = 195;
+
+                paddle1Y = 170;
+                paddle2Y = 170;
+            }
+            else if (ballX > 600)
+            {
+                player1Score++;
+
+                p1ScoreLabel.Text = $"{player1Score}";
+
+                ballX = 295;
+                ballY = 195;
+
+                paddle1Y = 170;
+                paddle2Y = 170;
+            }
+
+            // check score and stop game if either player is at 3 
+            if (player1Score == 3 || player2Score == 3)
+            {
+                gameTimer.Enabled = false;
+            }
+
+
+            Refresh();
+        }
+    }
+}
